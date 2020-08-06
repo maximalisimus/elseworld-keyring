4be3a96846506b1c260fe71cd7fbc4e586b4fa85
Mikhail Artamonov <maximalis171091@yandex.ru>
2020-08-06 22:34:05 +0300

    Update

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
 # maximalisimus keyring
 # https://maximalisimus.github.io/elseworld-keyring/
 
-$ sudo pacman-key --init
+$ wget https://maximalisimus.github.io/elseworld-keyring/install-keyring.sh
 
-$ wget https://maximalisimus.github.io/elseworld-keyring/elseworld{.gpg,-revoked,-trusted,-id.txt}
-
-$ sudo mv elseworld{.gpg,-revoked,-trusted} /usr/share/pacman/keyrings/
-
-$ sudo pacman-key --add /usr/share/pacman/keyrings/elseworld.gpg
-
-$ ewid=$(cat elseworld-id.txt)
-
-$ sudo pacman-key --lsign-key ${ewid}
-
-$ sudo pacman-key --populate archlinux elseworld
-
-$ sudo pacman-key --refresh-keys
-
-$ rm -rf elseworld-id.txt
+$ sudo sh install-keyring.sh
 
--- install-keyring.sh
+++ install-keyring.sh
@@ -5,17 +5,17 @@ sudo pacman-key --init
 wait
 wget https://maximalisimus.github.io/elseworld-keyring/elseworld{.gpg,-revoked,-trusted,-id.txt}
 wait
-mv elseworld{.gpg,-revoked,-trusted} /usr/share/pacman/keyrings/
+sudo mv elseworld{.gpg,-revoked,-trusted} /usr/share/pacman/keyrings/
 wait
-pacman-key --add /usr/share/pacman/keyrings/elseworld.gpg
+sudo pacman-key --add /usr/share/pacman/keyrings/elseworld.gpg
 wait
 ewid=$(cat elseworld-id.txt)
 wait
-pacman-key --lsign-key ${ewid}
+sudo pacman-key --lsign-key ${ewid}
 wait
-pacman-key --populate archlinux elseworld
+sudo pacman-key --populate archlinux elseworld
 wait
-pacman-key --refresh-keys
+sudo pacman-key --refresh-keys
 wait
 rm -rf elseworld-id.txt
 exit 0
